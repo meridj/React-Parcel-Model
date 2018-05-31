7b3f2db2dd4ba86c194eec5eda1871fb42dfada1
Mehdi Meridja <mehdi.meridj@gmail.com>
2018-05-31 13:52:15 +0200

    react install

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-const add = (n1, n2) => n1 + n2;
+/**
+ * Packages
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * Styles
+ */
+import './styles/reset.css';
+import './styles/index.css';
+
+const App = () => <h1>React-Parcel-Model</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
