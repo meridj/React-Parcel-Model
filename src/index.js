ed18050b11614b98b961a64b6a96b4514887c623
Mehdi Meridja <mehdi.meridj@gmail.com>
2018-05-31 14:13:52 +0200

    boilerplate done

--- src/index.js
+++ src/index.js
@@ -11,5 +11,6 @@ import './styles/reset.css';
 import './styles/index.css';
 
 const App = () => <h1>React-Parcel-Model</h1>;
+const targetRootNode = document.getElementById('root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, targetRootNode);
